b2c56f78c5ae35e60ae29c218413e05db952445c
Mustafa Uygur <mustafauygur@outlook.com>
2022-07-19 15:26:26 +0300

    Singleton has been added.

---
+++ Singleton/Instance.cs
@@ -0,0 +1,21 @@
+﻿namespace Singleton
+{
+    internal class Instance
+    {
+        private static Instance _instance;
+        private static readonly object _lock = new object();
+        private  Instance() {}
+
+        internal static Instance CreateInstance()
+        {
+            lock (_lock)
+            {
+                if (_instance == null)
+                {
+                    _instance = new Instance();
+                }
+                return _instance;
+            }
+        }
+    }
+}
